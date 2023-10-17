060e562bb4e4670b605548c7bd47727cf7934d26
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-10-17 12:57:31 -0300

    Complete implementation

--- scripts/format.bash
+++ scripts/format.bash
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-shfmt --language-dialect bash --write \
-	./**/*
+shfmt --language-dialect bash \
+  --indent 2 \
+  --case-indent \
+  --binary-next-line \
+  --write \
+  ./**/*
+
+# Options:
+# https://github.com/mvdan/sh/blob/master/cmd/shfmt/shfmt.1.scd
